9fc420f2c4c9b1fa06643c2a00ea8baa651a2e90
Nihal-pm0 <nihaluliyil@gmail.com>
2025-10-20 20:09:33 +0530

    pr.image

--- r.t/rt/rt/settings.py
+++ r.t/rt/rt/settings.py
@@ -159,17 +159,18 @@ AWS_S3_SIGNATURE_NAME = 's3v4'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = 'public-read' 
 AWS_S3_VERIFY = True
+AWS_QUERYSTRING_AUTH = False
 
 if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-    print("Using AWS S3 for media files")
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+    
 else:
-    # Local media files
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    print("Using local media files")
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
