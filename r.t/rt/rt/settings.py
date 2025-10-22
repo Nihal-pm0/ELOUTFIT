3390400909911a7f230e11049afc293303155990
Nihal-pm0 <nihaluliyil@gmail.com>
2025-10-22 17:16:41 +0530

    storage

--- r.t/rt/rt/settings.py
+++ r.t/rt/rt/settings.py
@@ -150,9 +150,15 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Media files
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
-
-# AWS S3 Configuration (only for production)
 # AWS S3 Configuration
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
