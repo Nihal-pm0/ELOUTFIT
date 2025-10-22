e494b91d65ad2e12eb16e6546d29fe113e1243c7
Nihal-pm0 <nihaluliyil@gmail.com>
2025-10-22 16:43:20 +0530

    media

--- r.t/rt/rt/settings.py
+++ r.t/rt/rt/settings.py
@@ -168,7 +168,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Media files on S3
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
